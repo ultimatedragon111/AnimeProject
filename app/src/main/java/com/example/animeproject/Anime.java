1019850e8daf415dceedb8cda037817faca82756
ultimatedragon111 <ultimatedragon111@gmail.com>
2022-02-15 20:17:22 +0100

    Botones funciona

--- app/src/main/java/com/example/animeproject/Anime.java
+++ app/src/main/java/com/example/animeproject/Anime.java
@@ -2,4 +2,67 @@ package com.example.animeproject;
 
 public class Anime {
 
+    String id;
+    String name;
+    String desc;
+    Type type;
+    String year;
+    String image;
+
+    Anime(String id,String name,String desc,Type type,String year,String image){
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.type = type;
+        this.year = year;
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
---
+++ app/src/main/java/com/example/animeproject/Type.java
@@ -0,0 +1,5 @@
+package com.example.animeproject;
+
+public enum Type {
+    TV,Film,OVA,Special
+}
