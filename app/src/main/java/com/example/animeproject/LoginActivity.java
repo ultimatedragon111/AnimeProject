405993b1135cc9804c3ddd4c15c33c442c56f70d
ultimatedragon111 <ultimatedragon111@gmail.com>
2022-02-15 18:33:26 +0100

    Botones funciona

--- app/src/main/java/com/example/animeproject/LoginActivity.java
+++ app/src/main/java/com/example/animeproject/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.animeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button1 =  findViewById(R.id.signUpButton1);
+        button2 =  findViewById(R.id.loginButton1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Intent activity2Intent = new Intent(getApplicationContext(), Singup.class);
+                startActivity(activity2Intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                //Intent a = new Intent(this,Singup.class);
+                //startActivity(a);
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/animeproject/Singup.java
+++ app/src/main/java/com/example/animeproject/Singup.java
@@ -2,13 +2,36 @@ package com.example.animeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Singup extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
+        button1 =  findViewById(R.id.signUpButton2);
+        button2 =  findViewById(R.id.loginButton2);
+        /*button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Intent a = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(a);
+            }
+        });*/
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Intent a = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }
